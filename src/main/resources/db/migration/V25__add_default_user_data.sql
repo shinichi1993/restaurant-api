bbb0ccd3cd10b0341481e49e96c7add9281cd0b5
Le Huu Ngat <shinichikaka@gmail.com>
2025-12-05 23:32:17 +0700

    Update V25__add_default_user_data.sql

--- src/main/resources/db/migration/V25__add_default_user_data.sql
+++ src/main/resources/db/migration/V25__add_default_user_data.sql
@@ -1,2 +1,2 @@
 INSERT INTO app_user(username,password,full_name,phone,role,status)
-VALUES ("ngat","ngat","ngat","0929292","ADMIN","ACTIVE")
+VALUES ('ngat','ngat','ngat','0929292','ADMIN','ACTIVE')
